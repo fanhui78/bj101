63ff83886c2b3679d90d88c73b69f69a454c9565
fanhui78 <fanhui78@qq.com>
2016-11-01 23:53:35 +0800

    11.1

--- models/user.js
+++ models/user.js
@@ -10,7 +10,84 @@ const UserSchema=new Schema({
     address:{type:String,default:''}
 
 });
-UserSchema.methods = {
 
+/**
+ * Virtuals;
+ */
+
+UserSchema
+    .virtual('password')
+    .set(function(password){
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function(){
+        return this._password;
+    });
+
+/**
+ * 方法
+ */
+UserSchema.methods = {
+    /**
+     * authenticate - 检查密码是否相同
+     * 
+     * @param {String} plainText
+     * @return {Boolean}
+     * @api public
+     * 
+     */
+    authenticate: function (plainText){
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    /**
+     * 生成salt
+     * 
+     * @return {String}
+     * @api public
+     * 
+     */
+    makeSalt:function(){
+        return Math.round((new Date().valueOf() * Math.random()))+'';
+    },
+
+    /**
+     * 加密密码
+     * 
+     * @param {String} password
+     * @return {String}
+     * @api public
+     */
+    encryptPassword:function(password){
+        if (!password) return '';
+        try{
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        }catch (err){
+            return '';
+        }
+    }
 };
+
+UserSchema.statics = {
+
+    /**
+     * 
+     * load
+     * 
+     * @param {Object} options
+     * @param {Function} callback
+     * @api private
+     */
+    load: function(options, callback){
+        options.select = options.select || 'username mobile';
+        return this.findOne(options.criteria)
+            .select(options.select)
+            .exec(callback);
+    }
+}
 mongoose.model('User',UserSchema);
